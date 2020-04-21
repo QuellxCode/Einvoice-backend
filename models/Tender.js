4b3e834e7424a9acc917ddb2199972ef2af80839
Daniyal Ahmed <daniyal.ahmed.177@gmail.com>
2020-04-21 16:10:03 +0500

    latest project

--- models/Tender.js
+++ models/Tender.js
@@ -2,12 +2,12 @@ const mongoose = require("mongoose");
 const Joi = require("joi");
 const TenderSchema = new mongoose.Schema({
   director_id: {
-    type: String
+    type: String,
   },
   engineers: [
     {
-      assigned_task: { type: String }
-    }
+      assigned_task: { type: String },
+    },
   ],
   status: { type: String, default: "Tender" },
   ongoing: { type: String, default: "draft" },
@@ -16,106 +16,132 @@ const TenderSchema = new mongoose.Schema({
       type: String,
       required: true,
       minlength: 3,
-      maxlength: 50
+      maxlength: 50,
     },
     type: {
       type: String,
       required: true,
       minlength: 3,
-      maxlength: 50
+      maxlength: 50,
     },
     client_name: { type: String, required: true, minlength: 3, maxlength: 50 },
     project_location: {
       type: String,
       required: true,
       minlength: 3,
-      maxlength: 50
+      maxlength: 50,
     },
     pec_category: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
   bid_bond: {
     bid_bond_fee: {
       type: Number,
-      required: true
+      required: true,
     },
     bid_bond_shape: {
       type: String,
-      required: true
+      required: true,
     },
     bid_bond_favor: {
       type: String,
-      required: true
-    }
+      required: true,
+    },
   },
+  doc_fee: {
+    bid_bond_doc_fee: {
+      type: Number,
+      required: true,
+    },
+    bid_bond_doc_shape: {
+      type: String,
+      required: true,
+    },
+    bid_bond_doc_favor: {
+      type: String,
+      required: true,
+    },
+  },
+
   tender: {
     tender_fee: {
       type: Number,
-      required: true
+      required: true,
     },
     tender_shape: {
       type: String,
-      required: true
+      required: true,
     },
     tender_favor: {
       type: String,
-      required: true
-    }
-  }
+      required: true,
+    },
+  },
+  doc_submission: {
+    doc_date: {
+      type: Number,
+      required: true,
+    },
+    doc_time: {
+      type: String,
+      required: true,
+    },
+    doc_venue: {
+      type: String,
+      required: true,
+    },
+  },
+  doc_purchase: {
+    purchase_date: {
+      type: Date,
+      required: true,
+    },
+    purchase_time: {
+      type: String,
+      required: true,
+    },
+    purchase_venue: {
+      type: String,
+      required: true,
+    },
+  },
 });
 const Tender = mongoose.model("Tender", TenderSchema);
 function validateTender(tender) {
   const schema = {
     engineers: Joi.array().items({
       engineer_id: Joi.string(),
-      assigned_task: Joi.string()
+      assigned_task: Joi.string(),
     }),
     details: Joi.object().keys({
-      work_title: Joi.string()
-        .min(3)
-        .max(50)
-        .required(),
-      type: Joi.string()
-        .min(3)
-        .max(50)
-        .required(),
-      client_name: Joi.string()
-        .min(3)
-        .max(50)
-        .required(),
-      project_location: Joi.string()
-        .min(3)
-        .max(50)
-        .required(),
-      pec_category: Joi.string()
-        .min(3)
-        .max(50)
-        .required()
+      work_title: Joi.string().min(3).max(50).required(),
+      type: Joi.string().min(3).max(50).required(),
+      client_name: Joi.string().min(3).max(50).required(),
+      project_location: Joi.string().min(3).max(50).required(),
+      pec_category: Joi.string().min(3).max(50).required(),
     }),
     bid_bond: Joi.object().keys({
       bid_bond_fee: Joi.number().required(),
-      bid_bond_shape: Joi.string()
-        .min(3)
-        .max(50)
-        .required(),
-      bid_bond_favor: Joi.string()
-        .min(3)
-        .max(50)
-        .required()
+      bid_bond_shape: Joi.string().min(3).max(50).required(),
+      bid_bond_favor: Joi.string().min(3).max(50).required(),
     }),
     tender: Joi.object().keys({
       tender_fee: Joi.number().required(),
-      tender_shape: Joi.string()
-        .min(3)
-        .max(50)
-        .required(),
-      tender_favor: Joi.string()
-        .min(3)
-        .max(50)
-        .required()
-    })
+      tender_shape: Joi.string().min(3).max(50).required(),
+      tender_favor: Joi.string().min(3).max(50).required(),
+    }),
+    doc_submission: Joi.object().keys({
+      doc_date: Joi.date().required(),
+      doc_time: Joi.string().min(3).max(50).required(),
+      doc_venue: Joi.string().min(3).max(50).required(),
+    }),
+    doc_purchase: Joi.object().keys({
+      purchase_date: Joi.date().required(),
+      purchase_time: Joi.string().min(3).max(50).required(),
+      purchase_venue: Joi.string().min(3).max(50).required(),
+    }),
   };
   return Joi.validate(tender, schema);
 }
